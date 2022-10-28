5bd38f014b48adfed481d6eb772e98c6720375fe
pauloh-alc <pauloalencar.aluno@gmail.com>
2022-10-28 17:29:32 -0300

    Criando classe URL
    Essa classe vai ser responsável por criar uma instância do tipo URL
    composta peos atributos name e href.
    
    * name: corresponde ao nome do arquivo que será baixado.
    * href: armazenará a URL para acesso ao PDF.

---
+++ src/main/java/br/com/paulo/entity/URL.java
@@ -0,0 +1,20 @@
+package br.com.paulo.entity;
+
+public class URL {
+	
+	private String name;
+	private String href;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getHref() {
+		return href;
+	}
+	public void setHref(String href) {
+		this.href = href;
+	}
+}
