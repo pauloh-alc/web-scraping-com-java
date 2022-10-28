0c9bb5e53dc17e1571ed0d39ab3da9ffc6d903fc
pauloh-alc <pauloalencar.aluno@gmail.com>
2022-10-28 15:48:31 -0300

    Criando método connectToThePage
    O método foi criado para se conectar a url fornecida, isto é, a url
    para o projeto. A partir dessa URL o método obtém todo documento HTML
    da página.

--- src/main/java/br/com/paulo/StartWebScrapingApplication.java
+++ src/main/java/br/com/paulo/StartWebScrapingApplication.java
@@ -1,13 +1,20 @@
 package br.com.paulo;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.paulo.service.WebScrapingService;
+
 @SpringBootApplication
 public class StartWebScrapingApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(StartWebScrapingApplication.class, args);
+	
+	public static  void main(String[] args) throws IOException {
+		SpringApplication.run(StartWebScrapingApplication.class, args);	
+		
+		WebScrapingService service = new WebScrapingService();
+		System.out.println(service.connectToThePage());
 	}
 
 }
--- src/main/java/br/com/paulo/service/WebScrapingService.java
+++ src/main/java/br/com/paulo/service/WebScrapingService.java
@@ -1,5 +1,17 @@
 package br.com.paulo.service;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class WebScrapingService {
 	
+	final static String URL = "https://www.gov.br/ans/pt-br/assuntos/consumidor/o-que-o-seu-plano-de-saude-deve-cobrir-1/o-que-e-o-rol-de-procedimentos-e-evento-em-saude";
+	
+	public Document connectToThePage() throws IOException {
+		Document documentHTML = Jsoup.connect(URL).get();
+		return documentHTML;
+	}
+
 }
